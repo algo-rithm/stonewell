3fbbb11a273ff608739bc0ec827036046f7ecd2f
StoneWell Studio <balanesipf@gmail.com>
2013-07-29 15:58:10 -0400

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stonewell::Application.config.secret_token = '007eedc8745423a698b91cd1191d7aeee82580b68d01505bd8df25714d60f11109bb72395091ab1b1aa04156bd41bab0575002e4161cdf9413108861ecd1ea44'
